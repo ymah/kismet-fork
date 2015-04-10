6ebc65496af02ed42e957e7daa7aaa1a23971cde
Mahieddine Yaker <mahieddine.yaker@gmail.com>
2015-04-10 20:54:06 +0200

    Amelioration du script d'enregistrement dans la base de donnée Elasticsearch

--- Test-ELK/script.py
+++ Test-ELK/script.py
@@ -6,30 +6,37 @@ from datetime import datetime
 import time
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch([{'host':'127.0.0.1'}])
+
+def getJsonFiles(path):
+    jsonfiles = [ f for f in listdir(path) if isfile(join(path,f)) ]
+    return jsonfiles
 
 
-PATH_LOG ='/var/log/kismet/'
+def dumpToElsch(jsonfiles,elschClient):
+    numberInsert = 0
+    numberFile = 0
+    for i in jsonfiles:
+        numberFile+=1
+        print("Traitemant du fichier : {0} ".format(i))
+        json_data=open(PATH_LOG+i)
+        data = json.load(json_data)
+        for f in data['Networks']:
+            pprint(f)
+            json_data.close()
+            post = f
+            elschClient.index(index='wifi_audit',doc_type='posts',id=numberInsert,body=post)
+            elschClient.indices.refresh(index='wifi_audit')
+            numberInsert+=1
+            print("{0} insertions".format(numberInsert))
+        print("Fin du traitement du fichier {0} ".format(i))
+        time.sleep(2)
+
+    print("{0} fichiers traités pour {1} insertions".format(numberFile,numberInsert))
 
 
-jsonfiles = [ f for f in listdir(PATH_LOG) if isfile(join(PATH_LOG,f)) ]
 
-numberInsert = 0
-numberFile = 0
-for i in jsonfiles:
-    numberFile+=1
-    print("Traitemant du fichier : {0} ".format(i))
-    json_data=open(PATH_LOG+i)
-    data = json.load(json_data)
-    for f in data['Networks']:
-        pprint(f)
-        json_data.close()
-        post = f
-        es.index(index='wifi_audit',doc_type='posts',id=numberInsert,body=post)
-        es.indices.refresh(index='wifi_audit')
-        numberInsert+=1
-        print("{0} insertions".format(numberInsert))
-    print("Fin du traitement du fichier {0} ".format(i))
-    time.sleep(2)
-
-print("{0} fichiers traités pour {1} insertions".format(numberFile,numberInsert))
+if __name__ == "__main__":
+    es = Elasticsearch([{'host':'127.0.0.1'}])
+    PATH_LOG ='/var/log/kismet/'
+    jsonfiles = getJsonFiles(PATH_LOG)
+    dumpToElsch(jsonfiles,es)
