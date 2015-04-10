1cf6ef751c0d45a94bed59d5df1f69b2fca2cf2c
Mahieddine Yaker <mahieddine.yaker@gmail.com>
2015-04-10 21:00:19 +0200

    ajout de commentaires explicatifs

--- Test-ELK/script.py
+++ Test-ELK/script.py
@@ -8,11 +8,20 @@ from elasticsearch import Elasticsearch
 
 
 def getJsonFiles(path):
+    """
+    Script permettant de récuperer les fichiers jsons à partir d'un
+    path fourni en parametre
+    """
     jsonfiles = [ f for f in listdir(path) if isfile(join(path,f)) ]
     return jsonfiles
 
 
 def dumpToElsch(jsonfiles,elschClient):
+    """
+    Script permettant de charger les données présentes dans la liste
+    de fichiers jsonfiles, dans la base de donnée elasticsearch fournie
+    avec le parametre elschClient
+    """
     numberInsert = 0
     numberFile = 0
     for i in jsonfiles:
@@ -24,18 +33,27 @@ def dumpToElsch(jsonfiles,elschClient):
             pprint(f)
             json_data.close()
             post = f
-            elschClient.index(index='wifi_audit',doc_type='posts',id=numberInsert,body=post)
+            elschClient.index(index='wifi_audit',doc_type='posts',\
+                              id=numberInsert,body=post)
             elschClient.indices.refresh(index='wifi_audit')
             numberInsert+=1
             print("{0} insertions".format(numberInsert))
         print("Fin du traitement du fichier {0} ".format(i))
         time.sleep(2)
 
-    print("{0} fichiers traités pour {1} insertions".format(numberFile,numberInsert))
+    print("{0} fichiers traités pour {1} insertions".format(numberFile,\
+                                                            numberInsert))
+
+
+
 
 
 
 if __name__ == "__main__":
+    """
+    programme main executant un enregistrement depuis le path
+    /var/log/kismet dans une base de donnée locale
+    """
     es = Elasticsearch([{'host':'127.0.0.1'}])
     PATH_LOG ='/var/log/kismet/'
     jsonfiles = getJsonFiles(PATH_LOG)
