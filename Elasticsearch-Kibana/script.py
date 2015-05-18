a271ffeae1d82257c1293fe74623ea45a12acc14
Mahieddine Yaker <mahieddine.yaker@gmail.com>
2015-05-18 10:30:49 +0200

    modification du script de chargement de donnée json pour prendre en compte les clients et les inserer en plus des reseaux (afin de contrer le probleme de Kibana)

--- Elasticsearch-Kibana/script.py
+++ Elasticsearch-Kibana/script.py
@@ -23,8 +23,13 @@ def getJsonFiles(path):
     jsonfiles = [ f for f in listdir(path) if (isfile(join(path,f)) and f.lower().endswith('.json')) ]
     return jsonfiles
 
+def merge_two_dicts(x, y):
+    '''Given two dicts, merge them into a new dict as a shallow copy.'''
+    z = x.copy()
+    z.update(y)
+    return z
 
-def dumpToElsch(jsonfiles,elschClient,index):
+def dumpToElsch(jsonfiles,elschClient,indexRsx,indexClient):
     """
     Script permettant de charger les données présentes dans la liste
     de fichiers jsonfiles, dans la base de donnée elasticsearch fournie
@@ -40,12 +45,22 @@ def dumpToElsch(jsonfiles,elschClient,index):
         for f in data['Networks']:
             pprint(f)
             json_data.close()
-            post = f
-            elschClient.index(index=index,\
+            network = f
+            elschClient.index(index=indexRsx,\
                               doc_type='posts',\
                               id=numberInsert,\
-                              body=post)
-            elschClient.indices.refresh(index=index)
+                              body=network)
+            elschClient.indices.refresh(index=indexRsx)
+            clients = f['details']['Clients']
+            for c in clients:
+                c = merge_two_dicts(c,{'MAC_HOST':f['details']['BSSID_MAC']})
+                elschClient.index(index=indexClient,\
+                                  doc_type='posts',\
+                                  id=numberInsert,\
+                                  body=c)
+                print("insertions de clients")
+                print(c)
+                elschClient.indices.refresh(index=indexClient)
             numberInsert+=1
             print("{0} insertions".format(numberInsert))
         print("Fin du traitement du fichier {0} ".format(i))
@@ -63,4 +78,4 @@ if __name__ == "__main__":
     es = Elasticsearch([{'host':'127.0.0.1'}])
     PATH_LOG = '/var/log/kismet/'
     jsonfiles = getJsonFiles(PATH_LOG)
-    dumpToElsch(jsonfiles,es,'wifi_audit')
+    dumpToElsch(jsonfiles,es,'reseaux','clients')
